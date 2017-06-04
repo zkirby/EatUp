1e49dc293b73962ff69b6608a0317e30576ba3b7
Zachary Kirby <ARK@JKillers-MBP.home>
2017-06-03 22:33:33 -0700

    house keeping -- updated some of the menu screen

--- src/pages/menu/menu.ts
+++ src/pages/menu/menu.ts
@@ -14,11 +14,38 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class MenuPage {
 
+	categories: object[];
+	items: object[];
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+  	this.categories = this.sampleDataCat();
+  	this.items = this.sampleDataItem();
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad MenuPage');
+  }
+
+  flipCard() {
+    console.log("card flipped");
+  }
+
+  sampleDataCat() {
+  	return [
+  		{ name: "drinks" }, 
+  		{ name: "entrees" }, 
+  		{ name: "sides" },
+  		{ name: "pastas" },
+  		{ name: "appetizers" }
+  	];
+  }
+
+  sampleDataItem() {
+  	return [
+  		{ name: "Pinot Grigio", subcontent: "Pairings: 1. Chicken Marsala, 2. Shrimp Scampi" },
+  		{ name: "Ginger Cream Soda", subcontent: "ginger, lemon, lime agave nectar creama" },
+  		{ name: "House Wine", subcontent: "not visable" } 
+  	];
   }
 
 }
